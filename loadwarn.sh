14cf72fe03a9138e0ec6614dff411d51d2eb7abb
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-11-08 16:10:16 +0100

    Update loadwarn.sh

--- loadwarn.sh
+++ loadwarn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the threshold for CPU load
-threshold=5
+threshold=$1
 
 # Get the 1-minute load average using the uptime command and extract the first value
 load_average=$(uptime | awk -F'[a-z]:' '{print $2}' | cut -d ',' -f 1)
@@ -10,13 +10,11 @@ load_average=${load_average%.*}  # Remove decimal part if present
 # Check if the load_average is greater than the threshold using integer comparison
 if [ "$load_average" -gt "$threshold" ]; then
     # Send an email
-    fromName="ImmoWarnings"
-    fromEmail="noreply@immozia.com"
+    fromName="LoadWarnings"
     subject="High CPU Load Alert"
-    recipient="test@immozia.com"
 
     message="CPU load is currently $load_average, which is higher than the threshold of $threshold."
 
     echo "Above threshold load !"
-    echo "$message" | mail -s "$subject" -aFrom:$fromName\<$fromEmail\> $recipient
+    echo "$message" | mail -s "$subject" -aFrom:$fromName\<$2\> $3
 fi
